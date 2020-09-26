ba517c5d93836b8e443e93c72e819813285b3709
Nikita Kharlap <harlap19@gmail.com>
2020-09-26 13:42:40 +0300

    Add hosting

---
+++ public/services/initbase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAC8SwOFVCHePLLjCzY2Ctt9NcBAHVy3xc",
+    authDomain: "calendar-2880f.firebaseapp.com",
+    databaseURL: "https://calendar-2880f.firebaseio.com",
+    projectId: "calendar-2880f",
+    storageBucket: "calendar-2880f.appspot.com",
+    messagingSenderId: "429024080141",
+    appId: "1:429024080141:web:8474cc21a19a5d80694044",
+    measurementId: "G-NBSVDCXX62"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
