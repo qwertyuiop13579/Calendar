a891d25617bb33e1e8956766e96efd1a2d1e16be
qwertyuiop13579 <49912768+qwertyuiop13579@users.noreply.github.com>
2020-09-28 14:20:16 +0300

    Add UI files

---
+++ views/components/Navbar.js
@@ -0,0 +1,46 @@
+let Navbar = {
+    render: async () => {
+        let view =  /*html*/`
+             <nav class="navbar" role="navigation" aria-label="main navigation">
+                <div class="container">
+                    <div  class="navbar-menu is-active" aria-expanded="false">
+                        <div class="navbar-start">
+                            <a class="navbar-item" href="/#/">
+                                Home
+                            </a>
+                            <a class="navbar-item" href="/#/appointments">
+                                Appointments
+                            </a>
+                            <a class="navbar-item" href="/#/profile">
+                                Profile
+                            </a>
+                            <a class="navbar-item" href="/#/about">
+                                About
+                            </a>                           
+                            <a class="navbar-item" href="/#/signin">
+                                Sign out
+                            </a>
+                        </div>
+                        <div class="navbar-end">
+                            <div class="navbar-item">
+                                <div class="buttons">
+                                    <a class="button is-primary" href="/#/register">
+                                        <strong>Sign up</strong>
+                                    </a>
+                                    <a class="button is-light" href="/#/signin">
+                                        Sign in
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        `
+        return view
+    },
+    after_render: async () => { }
+
+}
+
+export default Navbar;
